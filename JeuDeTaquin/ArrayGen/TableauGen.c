e7e66a3d486da181e717eda12678eefe73da2de1
WalkedKO <149374431+WalkedKO@users.noreply.github.com>
2024-01-08 10:35:04 +0100

    Change from .h to .c

--- JeuDeTaquin/ArrayGen/TableauGen.c
+++ JeuDeTaquin/ArrayGen/TableauGen.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "TableauStructure.h"
+#include "TableauStructure.c"
 #include <math.h>
 #define MAXDIGITS 10
 #define DIGITS_OF_STARTING_NUMBERS 2
--- JeuDeTaquin/ArrayGen/test.c
+++ JeuDeTaquin/ArrayGen/test.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "TableauGen.h"
+#include "TableauGen.c"
 
 main()
 {
