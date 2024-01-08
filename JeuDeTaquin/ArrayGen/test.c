518de54db4ac126923e1a74d2bcde0ca2f200c6f
WalkedKO <149374431+WalkedKO@users.noreply.github.com>
2024-01-08 19:16:39 +0100

    Changed test.c

--- JeuDeTaquin/ArrayGen/test.c
+++ JeuDeTaquin/ArrayGen/test.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "TableauGen.h"
+#include "RandomSetStruct.h"
+#include "TableauStructure.h"
 
 main()
 {
 	struct Tableau tab;
+	struct RandomSet rset;
 	tab.startingNr = 0.23;
-	tab.setSize = 9;
+	rset.setSize = 9;
 	tab.numberOfRows = 0;
-	tab.set = (float*)malloc(tab.setSize * sizeof(float));
-	tab.set[0] = 0.23;
+	rset.set = (float*)malloc(rset.setSize * sizeof(float));
+	rset.set[0] = tab.startingNr;
 
 
 	/*float sett[] = {.23, .35,.235,.23,.325,.876,.54,.8,.976,.45,.35};
