73f62950585a6f843397570a35959e186e1b4307
Tajfun403 <Tajfun403@gmail.com>
2024-01-09 01:31:18 +0100

    Some TODOs

--- JeuDeTaquin/BatchRunners.c
+++ JeuDeTaquin/BatchRunners.c
@@ -1,5 +1,6 @@
 #include "BatchRunners.h"
 #include <stdlib.h>
+#include "Graph/GraphItem.h"
 
 char* GenerateTables(int size, int count)
 {
@@ -8,5 +9,8 @@ char* GenerateTables(int size, int count)
 
 char* AnalyzeTables(char* source)
 {
+	// TODO get amount of files
+	// TODO create an array of GraphItems where the results will be saved for every run
+	// TODO then feed that table into GenerateGraph();
 	return NULL;
 }
