709301a443712d8e35bb2791fe1a5a9e9e08f96f
theonetrueMax <148761331+theonetrueMax@users.noreply.github.com>
2024-01-12 17:42:58 +0100

    Update main.c

--- JeuDeTaquin/ArrayAnalyze/main.c
+++ JeuDeTaquin/ArrayAnalyze/main.c
@@ -2,9 +2,10 @@
 #include "ArrayAnalyze.h"
 
 int main() {
-	char* path = "filename.txt";
+    
+    char* path = "filename.txt";
 
     printf("%d",SolveTableau(LoadTableauFromFile(path)));
 
-	return 0;
+    return 0;
 }
