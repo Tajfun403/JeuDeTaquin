a2e59a3fd3b3333cb9cb1a439790c9cca665174d
WalkedKO <149374431+WalkedKO@users.noreply.github.com>
2024-01-15 19:12:44 +0100

    Comments in header files

--- JeuDeTaquin/ArrayGen/TableauGen.h
+++ JeuDeTaquin/ArrayGen/TableauGen.h
@@ -1,6 +1,30 @@
 #pragma once
-
+/// <summary>
+/// Saves one tab to a separate file
+/// FILE STRUCTURE:
+/// 1st line: Magic number
+/// 2nd line: how many rows\n
+/// 3rd line: length od the longest row\n
+/// rest: tableau
+/// </summary>
+/// <param name="tab"></param>
+/// <param name="path"></param>
 void SaveTableau(struct Tableau tab, char path[]);
+/// <summary>
+/// Prints single row
+/// </summary>
+/// <param name="row"></param>
+/// <param name="size"></param>
 void PrintRow(float row[], int size);
+/// <summary>
+/// Prints all the tableau
+/// </summary>
+/// <param name="tab"></param>
 void PrintTableau(struct Tableau tab);
+/// <summary>
+/// Main generating process
+/// </summary>
+/// <param name="startingNum"></param>
+/// <param name="setSize"></param>
+/// <returns></returns>
 struct Tableau* GenerateTableau(double startingNum, int setSize);
--- JeuDeTaquin/ArrayGen/TableauStructure.h
+++ JeuDeTaquin/ArrayGen/TableauStructure.h
@@ -25,11 +25,63 @@ i copy that array itd
 why?
 because i want to move on and ill repair it later*/
 
+/// <summary>
+/// Resizes a single row
+/// </summary>
+/// <param name="row"></param>
+/// <param name="size"></param>
+/// <returns></returns>
 float* ResizeRow(float* row, int* size);
+/// <summary>
+/// Resizes an array which holds the sizes
+/// </summary>
+/// <param name="sizes"></param>
+/// <param name="currentRowsCounter"></param>
+/// <returns></returns>
 int* ResizeSizesArray(int* sizes, int currentRowsCounter);
+/// <summary>
+/// Resizes 2D array called Tableau 
+/// </summary>
+/// <param name="tableau"></param>
+/// <param name="numberOfRows"></param>
+/// <returns></returns>
 float** ResizeTableau(float** tableau, int* numberOfRows);
+/// <summary>
+/// Generates random set
+/// </summary>
+/// <param name="set"></param>
+/// <param name="size"></param>
 void GenerateRandomSet(float set[], int size);
+/// <summary>
+/// Calculates difference between n numbers in range (0,1) 
+/// </summary>
+/// <param name="howMany"></param>
+/// <returns></returns>
 float CalculateDelta(int howMany);
+/// <summary>
+/// Generates starting numbers from Delta, the starting numbers used in creating the graph
+/// </summary>
+/// <param name="delta"></param>
+/// <param name="howManyNumbers"></param>
+/// <returns></returns>
 float* GenerateStartingNumbers(float delta, int howManyNumbers);
+/// <summary>
+/// Finds the 2nd max element in a row
+/// </summary>
+/// <param name="row"></param>
+/// <param name="newElement"></param>
+/// <param name="rowSize"></param>
+/// <returns></returns>
 float* FindThe2ndMaxElement(float* row, float newElement, int* rowSize);
+
+/// <summary>
+/// In the tableau generating process, it throws the 2nd max element to the next row
+/// </summary>
+/// <param name="row"></param>
+/// <param name="element"></param>
+/// <param name="rowSize"></param>
+/// <param name="elementToThrowOut"></param>
+/// <returns></returns>
 float* ThrowElementToRow(float* row, float element, int* rowSize, float* elementToThrowOut);
+
+
