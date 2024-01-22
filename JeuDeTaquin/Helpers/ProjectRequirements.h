de88cbcd100ecf52321c3316940762d9ab05c17f
Tajfun403 <Tajfun403@gmail.com>
2024-01-22 01:39:50 +0100

    Wording

--- JeuDeTaquin/Helpers/ProjectRequirements.h
+++ JeuDeTaquin/Helpers/ProjectRequirements.h
@@ -1,7 +1,7 @@
 #pragma once
 
-// A switch between optimized (with real life sense) and highly questionable (no real life sense) 
-// function implementations, as the latter were forced by uni's project requirements
+// A switch between optimized (with real life sense) andquestionable
+// function implementations, as the latter were forced by some of uni's project requirements
 // CURRENTLY AFFECTS: 
 // ArrayAnalyze/ArrayAnalyze.c -> SolveTableau (in-place vs recursion)
 // ArrayGen/TableauStructure.c -> FindThe2ndMaxElement (linear search vs qsort)
